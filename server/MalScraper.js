a3f6b2e4ab12992b09b0d281afd643eb08c8b2e9
James-Oswald <jamesoswald111@gmail.com>
2020-06-03 22:06:11 -0400

    uprading the scraper

--- server/MalScraper.js
+++ server/MalScraper.js
@@ -1,37 +1,50 @@
+/*I really wanted to make my own web scraper rather then use the MAL Scraper some one else already made,
+however I'll probably end up forking this project and creating a new version without this custom one for
+a speed comparison at some point*/
 
 const https = require("https");
 const jsdom = require("jsdom");
-const {JSDOM} = jsdom;
+const { JSDOM } = jsdom;
 
 
-function processPage(HTMLString){
-    let doc = new JSDOM(HTMLString);
-    console.log(doc.window.document.title);
+//function sliceName
+
+function processPage(HTMLString, id) {
+    let document = new JSDOM(HTMLString).window.document;
+    let infobox = document.getElementsByClassName("borderClass")[0].children[0];
+    let scrapeData = {}
+    scrapeData["img"] = infobox.children[0].children[0].children[0].attributes["data-src"].value;
+    for (let i = 0; i < infobox.children.length; i++) {
+        let text = infobox.children[i].textContent.trim();
+        if (/[a-zA-Z]+:\s.+/.test(text)) {
+            let index = text.indexOf(":");
+            scrapeData[text.substring(0, index)] = text.substring(index + 1, text.length).trim();
+        }
+    }
+
+    return rv;
 }
 
-function scrape(AnimeId, callback, delay = 1){
+function scrape(AnimeId) { //, callback, delay = 1){
     let url = "https://myanimelist.net/anime/" + AnimeId + "/";
-    let req = https.request(url, function(responce){
+    let req = https.request(url, function(responce) {
         let data = "";
         let status = responce.statusCode;
-        if(status == 200){
+        if (status == 200) {
             responce.setEncoding("utf8");
-            responce.on("data", function(chunk){data += chunk;});
-            responce.on("end", function(){
-                processPage(data);
+            responce.on("data", function(chunk) { data += chunk; });
+            responce.on("end", function() {
+                processPage(data, AnimeId);
             });
-        }else{
+        } else {
             console.warn("Status Code " + status);
         }
     })
-    req.on("error", function(err){
+    req.on("error", function(err) {
         console.error(err);
     });
     req.end();
     //callback();
 }
 
-scrape(40591, function(){});
-//scrape(40591, function(){});
-//scrape(40591, function(){});
-//scrape(40591, function(){});
+scrape(40591);
